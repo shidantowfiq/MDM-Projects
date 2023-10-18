9cd9e1473faed2cb2406fccf776b2c3ab4076b66
SHTO <shto@stibo.com>
2023-10-18 16:41:56 -0400

    Committed from STEP 2023-10-18T20:41Z

---
+++ pmdm-us/cp20231018-MixedBRsProducts/Primary Items/BusinessRule/BusinessRule_GetParent.js
@@ -0,0 +1,40 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "GetParent",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "Functions" ],
+  "name" : "Get Parent",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessFunctionWithBinds",
+  "binds" : [ ],
+  "messages" : [ ],
+  "pluginType" : "Operation",
+  "functionReturnType" : "com.stibo.core.domain.Node",
+  "functionParameterBinds" : [ {
+    "contract" : "NodeBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : ""
+  } ]
+}
+*/
+exports.operation0 = function (node) {
+return node.getParent();
+}
