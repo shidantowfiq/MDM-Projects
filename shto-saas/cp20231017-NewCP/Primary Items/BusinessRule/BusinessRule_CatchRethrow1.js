9dd81c31c0cbb73a6a73a242a7b3e4b8bb0c1670
SHTO <shto@stibosystems.com>
2023-10-27 12:10:12 -0700

    Committed from STEP 2023-10-27T19:10Z

---
+++ shto-saas/cp20231017-NewCP/Primary Items/BusinessRule/BusinessRule_CatchRethrow1.js
@@ -0,0 +1,55 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "CatchRethrow1",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "CatchRethrow1",
+  "description" : "RD-20365",
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager,logger,node) {
+var classification = manager.getClassificationHome().getClassificationByID("ConfigurationsRoot");
+try {
+	classification.removeAsset(null);
+} catch (e) {
+	logger.info("Attempting to ignore exception: " + e.javaException);
+}
+}
